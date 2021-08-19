efca63a36d937e982d95fadefe18e0324fe210d3
Dalton Walker <dalton_walker@icloud.com>
2021-08-19 17:08:52 -0400

    guess whos back, back again ... its termcolor

--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
+version = 1.0
+
+
 # -- Imports -- #
+import pip
 import os 
 import sys
 from subprocess import run, PIPE
 import yaml
 
-version = 1.0
+
+# -- pip install things -- #
+def pip_install(package):
+    pip.main(['install', package])
+
+try: 
+  from termcolor import colored
+except ImportError:
+    pip_install('termcolor')
+    from termcolor import colored
 
 
 def print_header(msg, index):
   print('\n')
-  print(str(index) + '. ' + msg)
-  print ('\n')
-
-
-def run_cmd(cmd):
-  return run(
-    cmd, 
-    shell=True, 
-    stdout=PIPE, 
-    stderr=PIPE, 
-    check=True).stdout.decode('utf-8')
-
+  print(colored(str(index) + '. ' + msg, 'blue', attrs=['bold']))
+  print('\n')
 
 # -- Introduction -- #
 title = """
@@ -49,8 +52,8 @@ The script will ask for some basic information to generate the necessary ssh key
 After that information is provided it will proceed without user input. 
 
 """
-print(title)
-print('version: ' + str(version))
+print(colored(title, 'green'))
+print(colored('version: ' + str(version), 'blue', attrs=['bold']))
 print(intro_text)
 
 
@@ -63,6 +66,15 @@ print(intro_text)
 # -- Gather necessary user information for Github ssh keys -- #
 print('Your GitHub email is required for setting up ssh properly. Enter the email you use for your GitHub account:')
 #github_email = input()
+
+
+def run_cmd(cmd):
+  return run(
+    cmd, 
+    shell=True, 
+    stdout=PIPE, 
+    stderr=PIPE, 
+    check=True).stdout.decode('utf-8')
 
 
 # -- Install zsh -- #
