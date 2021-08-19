1370b924e3c019acdfec14cc08103d9b91f8ffeb
Dalton Walker <dalton_walker@icloud.com>
2021-08-19 19:03:47 -0400

    added $HOME

--- setup.py
+++ setup.py
@@ -109,27 +109,27 @@ print_header('Installing fonts', 1, 'blue')
 check_and_install_pkg('wget')
 run('mkdir firacode_nf')
 run('mkdir firamono_nf')
-run('mkdir ~/.local/share/fonts')
+run('mkdir $HOME/.local/share/fonts')
 
 # Install FiraCode NerdFont
 url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip"
 cmd = 'wget ' + url
 run(cmd)
 run('unzip FiraCode.zip -d firacode_nf')
-run('cp firacode_nf/Fira\ Code\ Bold\ Nerd\ Font\ Complete.otf ~/.local/share/fonts/')
-run('cp firacode_nf/Fira\ Code\ Light\ Nerd\ Font\ Complete.otf ~/.local/share/fonts/')
-run('cp firacode_nf/Fira\ Code\ Medium\ Nerd\ Font\ Complete.otf ~/.local/share/fonts/')
-run('cp firacode_nf/Fira\ Code\ Regular\ Nerd\ Font\ Complete.otf ~/.local/share/fonts/')
-run('cp firacode_nf/Fira\ Code\ Retina\ Nerd\ Font\ Complete.otf ~/.local/share/fonts/')
+run('cp firacode_nf/Fira\ Code\ Bold\ Nerd\ Font\ Complete.otf $HOME/.local/share/fonts/')
+run('cp firacode_nf/Fira\ Code\ Light\ Nerd\ Font\ Complete.otf $HOME/.local/share/fonts/')
+run('cp firacode_nf/Fira\ Code\ Medium\ Nerd\ Font\ Complete.otf $HOME/.local/share/fonts/')
+run('cp firacode_nf/Fira\ Code\ Regular\ Nerd\ Font\ Complete.otf $HOME/.local/share/fonts/')
+run('cp firacode_nf/Fira\ Code\ Retina\ Nerd\ Font\ Complete.otf $HOME/.local/share/fonts/')
 
 # Install FiraMono NerdFont
 url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraMono.zip"
 cmd = 'wget ' + url
 run(cmd)
 run('unzip FiraMono.zip -d firamono_nf')
-run('cp firamono_nf/Fira\ Mono\ Bold\ Nerd\ Font\ Complete.otf ~/.local/share/fonts/')
-run('cp firamono_nf/Fira\ Mono\ Medium\ Nerd\ Font\ Complete.otf ~/.local/share/fonts/')
-run('cp firamono_nf/Fira\ Mono\ Regular\ Nerd\ Font\ Complete.otf ~/.local/share/fonts/')
+run('cp firamono_nf/Fira\ Mono\ Bold\ Nerd\ Font\ Complete.otf $HOME/.local/share/fonts/')
+run('cp firamono_nf/Fira\ Mono\ Medium\ Nerd\ Font\ Complete.otf $HOME/.local/share/fonts/')
+run('cp firamono_nf/Fira\ Mono\ Regular\ Nerd\ Font\ Complete.otf $HOME/.local/share/fonts/')
 
 # Clean up
 run('rm FiraCode.zip')
