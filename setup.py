7c70b39ea3422c0ad8ab076f3ce03a058073d5b8
Dalton Walker <dalton_walker@icloud.com>
2021-08-19 11:47:00 -0400

    removed colored

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 # -- Imports -- #
-from termcolor import colored
 import os 
 import sys
+import subprocess
 
 version = 1.0
+
+def print_header(msg, index):
+  print('\n')
+  print(str(index) + '. ' + msg)
+  print ('\n')
+
 
 # -- Introduction -- #
 title = """
@@ -32,15 +38,28 @@ The script will ask for some basic information to generate the necessary ssh key
 After that information is provided it will proceed without user input. 
 
 """
-print(colored(title, 'green'))
-print(colored('version: ' + str(version), 'blue', attrs=['bold']))
+print(title)
+print('version: ' + str(version))
 print(intro_text)
 
 
 # -- Check for root privileges -- # 
-if os.geteuid() != 0: 
-  print(colored('This script requires root privileges to operate properly.', 'red', attrs=['bold'])) 
-  sys.exit()
+#if os.geteuid() != 0: 
+#  print(colored('This script requires root privileges to operate properly.', 'red', attrs=['bold'])) 
+#  sys.exit()
 
 
 # -- Gather necessary user information for Github ssh keys -- #
+print('Your GitHub email is required for setting up ssh properly. Enter the email you use for your GitHub account:')
+#github_email = input()
+
+
+# -- Install zsh -- #
+print_header('Installing zsh', 1, 'green')
+
+print('Gathering prerequisites ...')
+cmd = ['apt list', '--installed']
+shell_out = subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8')
+print(shell_out)
+
+
