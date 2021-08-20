8c4b1c6c7531dcb15c2948421567d279c74e09c0
Dalton Walker <dalton_walker@icloud.com>
2021-08-19 20:53:21 -0400

    asdsadas

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ def check_and_install_pkg(pkg):
 
 # -- Install pyenv -- #
 cmd = """
-apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+sudo apt-get update; sudo apt-get -y install make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
